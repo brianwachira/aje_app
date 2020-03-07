8c0a8a39674ad0fe831fbb71bf5f242520f50a78
brianwachira <brianwachira7@gmail.com>
2020-03-07 13:00:40 +0300

    add contactGroup entity

--- app/src/main/java/com/example/ex_contactapp/data/Entities/ContactGroup.java
+++ app/src/main/java/com/example/ex_contactapp/data/Entities/ContactGroup.java
@@ -1,4 +1,49 @@
-package com.example.ex_contactapp.data.entities;
+package com.example.ex_contactapp.data.Entities;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import static androidx.room.RoomMasterTable.TABLE_NAME;
+
+@Entity (tableName = TABLE_NAME)
 public class ContactGroup {
+
+    public static final String TABLE_NAME = "contactgroup";
+
+    @PrimaryKey(autoGenerate = true) @NonNull
+    private Integer groupid;
+
+    @NonNull
+    private String groupname;
+
+    @NonNull
+    private String numofcontacts;
+
+
+    public Integer getGroupid() {
+        return groupid;
+    }
+
+    public void setGroupid(Integer groupid) {
+        this.groupid = groupid;
+    }
+
+    public String getGroupname() {
+        return groupname;
+    }
+
+    public void setGroupname(String groupname) {
+        this.groupname = groupname;
+    }
+
+    public String getNumofcontacts() {
+        return numofcontacts;
+    }
+
+    public void setNumofcontacts(String numofcontacts) {
+        this.numofcontacts = numofcontacts;
+    }
+
 }
+
